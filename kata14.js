50c0889c4d03c358e04e9cd8401d6e15c3348fc1
AlinaDev252 <costachealina25@gmail.com>
2021-02-18 21:27:31 +0200

    Add Kata14 - Fizz Buzz

---
+++ kata14.js
@@ -0,0 +1,7 @@
+function fizzbuzz(n) {
+	return Array.apply(null, new Array(n)).map(function (e, i) {
+		return (++i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i;
+	});
+}
+
+console.log(JSON.stringify(fizzbuzz(10)));
